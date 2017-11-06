aa4efa9eb5a85269b6152a33902587c6beeb4ea1
Ramprakash Munugapati <ramprakash.munugapati@in.ibm.com>
2017-11-06 16:23:29 +0530

    variables example.

---
+++ functionDef.py
@@ -0,0 +1,9 @@
+def functionDefination(n):
+    if(n==1):
+        return "one"
+    else:
+        return "other"
+
+print(functionDefination(0))
+
+
--- whileloop.py
+++ whileloop.py
@@ -6,4 +6,3 @@ while b < 50:
     print(b)
     a, b = b, a + b
 print("done fibanoci")
-    
