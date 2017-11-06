2cdb8b056628f04dac6234e0a1ef34927753d994
Ramprakash Munugapati <ramprakash.munugapati@in.ibm.com>
2017-11-06 16:09:33 +0530

    variables example.

---
+++ variables.py
@@ -0,0 +1,14 @@
+than=100
+print(than)
+
+meters= 24
+total_metars =than * meters
+print("Total Meters Merchant have :",total_metars)
+
+than_type1="cotton"
+than_type2="polyster"
+than_type3="silk"
+print("Cloth Categories : ", than_type1,than_type2,than_type3)
+
+categories=[than_type1,than_type2,than_type3]
+print(categories)
